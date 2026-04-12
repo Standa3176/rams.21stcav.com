df8aaa21bc9a44d15f6c544d1ae0aefcb0993923
SONNY.TANDA <SONNY@21STCENTURYAV.COM>
2026-04-12 19:39:08 +0100

    feat(07-04): public answer endpoint — route + PublicSurveyController::answerQuestion()
    - Added POST survey/{token}/rooms/{room}/questions/{question} route (throttle:120,1)
    - Added answerQuestion() to PublicSurveyController with token+room+question scope gates
    - Security: first()+abort_if(null,403) instead of firstOrFail() to avoid leaking question ID existence
    - All 6 PublicSurveyQuestionAnswerTest tests GREEN

--- rams.21stcav.com/routes/web.php
+++ rams.21stcav.com/routes/web.php
@@ -52,6 +52,9 @@ Route::post('survey/{token}/submit',                   [PublicSurveyController::
 Route::post('survey/{token}/rooms/{room}/photos',      [PublicSurveyController::class, 'uploadPhoto'])    ->name('survey.photos.upload')   ->middleware('throttle:30,1');
 Route::post('survey/{token}/rooms/{room}/complete',    [PublicSurveyController::class, 'completeRoom'])   ->name('survey.room.complete')   ->middleware('throttle:60,1');
 Route::post('survey/{token}/rooms/{room}/uncomplete',  [PublicSurveyController::class, 'uncompleteRoom']) ->name('survey.room.uncomplete') ->middleware('throttle:60,1');
+Route::post('survey/{token}/rooms/{room}/questions/{question}', [PublicSurveyController::class, 'answerQuestion'])
+    ->name('survey.question.answer')
+    ->middleware('throttle:120,1');
 Route::get('survey/{token}/photos/{photo}',            [PublicSurveyController::class, 'servePhoto'])     ->name('survey.photos.serve');
 
 /*
